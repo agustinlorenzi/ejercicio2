0e8156e3b804f178a6b9ffb32e977fec6552966d
Agustin Lorenzi <agustinlorenzi6@gmail.com>
2022-12-23 14:00:29 -0300

    ejercicio2 - corregido

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 Realizar la creación y carga del vector en el constructor. Crear un método para imprimir el vector.*/
 
 class Operario {
-    constructor(sueldos) {        
+    constructor(sueldos) {
         this.sueldos = sueldos
     }
     mostrarDatos() {
@@ -20,37 +20,50 @@ let sueldos = []
 let operarioSueldo = 0;
 let operario = document.getElementById("escuchar");
 operario.addEventListener("input", () => {
-    operarioSueldo = parseFloat(operario.value)    
+    operarioSueldo = parseFloat(operario.value)
 });
+
 let conteo = 0
 let quedan = 5
+
 let padre3 = document.getElementById("cuenta")
 padre3.innerHTML = `<p>Faltan Cargar ${quedan} Sueldo/s</p>`
+
 function cargarSueldo() {
-    conteo++
-    if (conteo <= 5) {
-        quedan--
-        padre3 = document.getElementById("cuenta")
-        padre3.innerHTML = `<p>Faltan Cargar ${quedan} Sueldo/s</p>`
-        sueldos.push(`Operario ${conteo} Sueldo de $ ` + operarioSueldo)
-        sueldoCargando(conteo,operarioSueldo)
-        operario.value = ``
-        operarioSueldo = 0
-    }else{
+    let padre2 = document.getElementById("avisar")
+    padre2.innerHTML = ``
+    if (operarioSueldo > 0 && operario.value !== "") {
+        conteo++
+        if (conteo <= 5) {
+            quedan--
+            padre3 = document.getElementById("cuenta")
+            padre3.innerHTML = `<p>Faltan Cargar ${quedan} Sueldo/s</p>`
+            sueldos.push(`Operario ${conteo} Sueldo de $ ` + operarioSueldo)
+            sueldoCargando(conteo, operarioSueldo)
+            operario.value = ``
+            operarioSueldo = 0
+        }
+        if (quedan === 0) {
+            const remuneracion = new Operario(sueldos);
+            remuneracion.mostrarDatos()
+            quedan = -1
+        }
+    } else if (conteo <= 4) {
         let padre2 = document.getElementById("avisar")
-        padre2.innerHTML = `<p>Ya se cargaron los 5 sueldos</p>`              
+        padre2.innerHTML = `<p>Coloque un valor.</p>`
+    } else {
+        let padre2 = document.getElementById("avisar")
+        padre2.innerHTML = `<p>Ya se cargaron los 5 sueldos</p>`
+
     }
-    if (quedan === 0) {
-        const remuneracion = new Operario(sueldos);
-        remuneracion.mostrarDatos()
-        quedan = -1           
 }
+
 
 function sueldoCargando(conteo, operarioSueldo) {
     Toastify({
-      text: "Se cargo el sueldo del operario " + conteo + " que es de un monto de $ " + operarioSueldo,
-      duration: 3000,
-      gravity: "top",
-      position: "right",
+        text: "Se cargo el sueldo del operario " + conteo + " que es de un monto de $ " + operarioSueldo,
+        duration: 3000,
+        gravity: "top",
+        position: "right",
     }).showToast();
-  }}
+}
